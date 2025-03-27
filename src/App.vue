16f7590ecfd301c86c85dd158e6705ea37f1ff3e
LuckyFoxCode <sergei181989@gmail.com>
2025-03-27 19:02:29 +0200

    feat: add AppContent component and integrate it into App.vue; update main.css for layout adjustments

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import AppContent from './components/AppContent.vue';
 import AppFooter from './components/AppFooter.vue';
 import AppHeader from './components/AppHeader.vue';
 </script>
 
 <template>
   <AppHeader />
+  <AppContent />
   <AppFooter />
 </template>
---
+++ src/components/AppContent.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="w-full flex-1 rounded-lg bg-white px-3 py-2 shadow-lg">Main</main>
+</template>
